a0b3e371195dd1865633d43135ff194fff98cb57
Dave86dev <davidochando1986@gmail.com>
2021-05-25 10:14:25 +0200

    Conexion a la database funcionando + servidor simultáneamente

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const db = require('./db.js');
 
 
-
+//Connecting to the database
+db.then(()=>{
 //Starting server
-app.listen(port, ()=> console.log(`Servidor levantado en http://localhost:${port}`));
+    app.listen(port, ()=> console.log(`Servidor levantado en http://localhost:${port}`));
+})
+.catch((err)=> console.log(err.message));   
